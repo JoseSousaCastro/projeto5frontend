a648c68b8f557726f6cf456e2cadac7bed081c9b
JoseSousaCastro <152998913+JoseSousaCastro@users.noreply.github.com>
2024-04-14 17:14:03 +0100

    dashboard aside

--- src/components/DashboardAside/DashboardAside.jsx
+++ src/components/DashboardAside/DashboardAside.jsx
@@ -1,3 +1,56 @@
 /* - Por cada utilizador, deverá ser possível consultar:
 	- Contagem total de tarefas atribuídas a este utilizador
 	- o Contagem do número de tarefas por estado (e.g. 5 em “DOING”, 2 em “DONE”, etc) */
+
+import React, { useEffect, useState } from "react";
+import { userStore } from "../../stores/UserStore";
+import { taskStore } from "../../stores/TaskStore";
+import "../DashboardAside/DashboardAside.css";
+
+function DashboardAside () {
+	const users = userStore(state => state.users);
+	const tasks = taskStore(state => state.tasks);
+	const [userTasks, setUserTasks] = useState([]);
+	const [userTasksToDo, setUserTasksToDo] = useState([]);
+	const [userTasksDoing, setUserTasksDoing] = useState([]);
+	const [userTasksDone, setUserTasksDone] = useState([]);
+	const [userTasksTotal, setUserTasksTotal] = useState(0);
+	const [userTasksToDoTotal, setUserTasksToDoTotal] = useState(0);
+	const [userTasksDoingTotal, setUserTasksDoingTotal] = useState(0);
+	const [userTasksDoneTotal, setUserTasksDoneTotal] = useState(0);
+
+	const handleChange = (event) => {
+		const username = event.target.value;
+		const userTasks = tasks.filter(task => task.owner.name === username);
+		const userTasksToDo = userTasks.filter(task => task.stateId === 100);
+		const userTasksDoing = userTasks.filter(task => task.stateId === 200);
+		const userTasksDone = userTasks.filter(task => task.stateId === 300);
+		setUserTasksTotal(userTasks.length);
+		setUserTasksToDoTotal(userTasksToDo.length);
+		setUserTasksDoingTotal(userTasksDoing.length);
+		setUserTasksDoneTotal(userTasksDone.length);
+	}
+
+return (
+	<div className="dashboardAside">
+		<div className="dashboard-aside-container">
+			<label>Task stats by user</label>
+			<select className="dropdown-select-dashboard" id="select-user" onChange={handleChange}>
+				<option value="" disabled selected>Select user</option>
+				{users.map((user) => (
+					<option key={user.username} value={user.username}>{user.firstName} {user.lastName}</option>
+				))}
+			</select>
+			<div className="user-tasks">
+				<p className="dashboardAside-total">Total Tasks: {userTasksTotal}</p>
+				<p className="dashboardAside-total-tasktype">To Do: {userTasksToDoTotal}</p>
+				<p className="dashboardAside-total-tasktype">Doing: {userTasksDoingTotal}</p>
+				<p className="dashboardAside-total-tasktype">Done: {userTasksDoneTotal}</p>
+			</div>
+		</div>
+	</div>
+	)
+
+}
+
+export default DashboardAside;
--- src/components/DashboardMain/DashboardMain.jsx
+++ src/components/DashboardMain/DashboardMain.jsx
@@ -8,3 +8,44 @@
 - Gráfico que mostre o número de utilizadores registados ao longo do tempo (e.g. gráfico de linhas). Os utilizadores apagados podem ser excluídos desta contagem.
 - Gráfico cumulativo que mostre o número total de tarefas concluídas ao longo do tempo. Tarefas apagadas podem ser excluídas desta contagem.
 */
+
+import React, { useEffect, useState } from "react";
+import { userStore } from "../../stores/UserStore";
+import { taskStore } from "../../stores/TaskStore";
+import "../DashboardMain/DashboardMain.css";
+
+function DashboardMain() {
+    const users = userStore(state => state.users);
+    const tasks = taskStore(state => state.tasks);
+    const [userTasks, setUserTasks] = useState([]);
+    const [userTasksToDo, setUserTasksToDo] = useState([]);
+    const [userTasksDoing, setUserTasksDoing] = useState([]);
+    const [userTasksDone, setUserTasksDone] = useState([]);
+    const [userTasksTotal, setUserTasksTotal] = useState(0);
+    const [userTasksToDoTotal, setUserTasksToDoTotal] = useState(0);
+    const [userTasksDoingTotal, setUserTasksDoingTotal] = useState(0);
+    const [userTasksDoneTotal, setUserTasksDoneTotal] = useState(0);
+
+    const handleChange = (event) => {
+        const username = event.target.value;
+        const userTasks = tasks.filter(task => task.owner.name === username);
+        const userTasksToDo = userTasks.filter(task => task.stateId === 100);
+        const userTasksDoing = userTasks.filter(task => task.stateId === 200);
+        const userTasksDone = userTasks.filter(task => task.stateId === 300);
+        setUserTasksTotal(userTasks.length);
+        setUserTasksToDoTotal(userTasksToDo.length);
+        setUserTasksDoingTotal(userTasksDoing.length);
+        setUserTasksDoneTotal(userTasksDone.length);
+    }
+
+    return (
+        <div className="DashboardMain">
+            <div className="dashboard-main-container">
+
+            </div>
+        </div>
+    )
+
+}
+
+export default DashboardMain;
